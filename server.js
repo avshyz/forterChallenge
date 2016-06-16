897da82f81a8c73b0c3ecb1eacf86f05e445b3b8
Avishay Zarad <zarad.avishay@gmail.com>
2016-06-17 00:25:22 +0300

    adding stylus

--- server.js
+++ server.js
@@ -1,19 +1,32 @@
 var express = require('express');
+var stylus = require('stylus');
+var nib = require('nib');
+
 var app = express();
+function compile(str, path) {
+    return stylus(str)
+        .set('filename', path)
+        .use(nib())
+}
+
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var port = 3000;
 
-app.use(express.static(__dirname + '/client'));
 app.set('views', __dirname + '/client/templates');
 app.set('view engine', "jade");
 app.engine('jade', require('jade').__express);
 
+app.use(stylus.middleware({src: __dirname + '/client', compile: compile}));
+app.use(express.static(__dirname + '/client'));
+
+
+
 require('./server/routes.js')(app);
 require('./server/ws_routes')(io);
 
-server.listen(3000, function(){
-  console.log('listening on *:3000');
+server.listen(3000, function () {
+    console.log('listening on *:3000');
 });
 
 console.log("Web server listening on port " + port);
