f9146bfbbab37ee5ad63518395110734ab03c19b
Avishay Zarad <zarad.avishay@gmail.com>
2016-06-17 11:54:44 +0300

    adding uniq colors

--- client/js/app.js
+++ client/js/app.js
@@ -8,28 +8,39 @@ app.controller('appController', ['$scope', function ($scope) {
     // restyle
     // refactor styles
 
-    $scope.log = [];
+    $scope.pick = function () {
+        return '#' + Math.floor(Math.random() * 16777215).toString(16);
+    };
 
-    sock.on('auth', function(data) {
+
+    $scope.log = [];
+    $scope.uniqColors = {};
+
+    sock.on('auth', function (data) {
         console.log(data);
         $scope.id = data.id;
         $scope.onlineUsers = data.onlineUsers;
+        $scope.uniqColors = _.reduce($scope.onlineUsers, function(res, item) {
+            res[item] = $scope.pick();
+            return res;
+        }, {});
         console.log($scope.onlineUsers);
-
         $scope.$apply();
     });
 
     sock.on('user_connected', function (data) {
         $scope.onlineUsers.push(data.id);
+        $scope.uniqColors[data.id] = $scope.pick();
         $scope.$apply();
     });
 
     sock.on('user_disconnected', function (data) {
         $scope.onlineUsers.splice($scope.onlineUsers.indexOf(data.id), 1);
+        delete $scope.uniqColors[data.id];
         $scope.$apply();
     });
 
-    sock.on('received', function(data) {
+    sock.on('received', function (data) {
         console.log(data);
         $scope.log.push(data);
         $scope.$apply()
