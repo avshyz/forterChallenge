4a566171b168ba7086c4fde2e04d2a37394fce99
Avishay Zarad <zarad.avishay@gmail.com>
2016-06-17 14:47:18 +0300

    adding math to AI

--- server/hal_bot.js
+++ server/hal_bot.js
@@ -1,3 +1,5 @@
+shunt = require('shunting-yard-arbitrary-precision');
+
 var Hal = function () {
     this.knowledge = {
         'what is the answer to life the universe and everything?': '42',
@@ -9,20 +11,39 @@ var Hal = function () {
     console.log(this.name + ' AWAKEN');
 };
 
-Hal.prototype.formalizeMessage = function(message) {
+Hal.prototype.respond = function (message) {
+    return {
+        author: this.name,
+        msg: message,
+        source: 'bot'
+    }
+};
+
+Hal.prototype.formalizeStringMessage = function (message) {
     return message.replace(/[^a-zA-Z0-9? ]/ig, '')
         .toLowerCase();
 };
 
-Hal.prototype.eavesdrop = function (message) {
-    message = this.formalizeMessage(message);
+Hal.prototype.formalizeArithmeticExpression = function (message) {
+    // must be a bug with the library, not recognizing parenthesis of different types
+    return message.replace(/[\[\(\{]/ig, '(')
+        .replace(/[\]\)\|]/ig, ')')
+};
+
+Hal.prototype.handleArithmeticExpression = function (message) {
+    message = this.formalizeArithmeticExpression(message);
+    try {
+        return this.respond(shunt(message));
+    } catch (err) {
+        return this.respond('I may not be an abacus, but you should work on your math');
+    }
+};
+
+Hal.prototype.handleStringExpression = function (message) {
+    message = this.formalizeStringMessage(message);
     if (message.endsWith('?')) {
         if (message in this.knowledge) {
-            return {
-                    author: this.name,
-                    msg: this.knowledge[message],
-                    source: 'bot'
-                };
+            return this.respond(this.knowledge[message]);
         } else {
             this.currentQuestion = message;
         }
@@ -35,6 +56,26 @@ Hal.prototype.eavesdrop = function (message) {
     }
 
     return '';
+};
+
+Hal.prototype.determineIfArithmeticExpression = function (message) {
+    // CHECK:
+    // 1. the message contains ONLY arithmetical characters
+    // 2. the message contains at least one operator
+    // 3. the message contains at least two numbers
+
+    return message.match(/^[\d.+-/\*\(\)\[\]\{\}\s]+$/) &&
+        message.match(/[+-/.\*)]/) &&
+        message.match(/\d+/g) &&
+        message.match(/\d+/g).length > 1;
+};
+
+Hal.prototype.eavesdrop = function (message) {
+    if (this.determineIfArithmeticExpression(message)) {
+        return this.handleArithmeticExpression(message);
+    } else {
+        return this.handleStringExpression(message);
+    }
 };
 
 module.exports = Hal;
